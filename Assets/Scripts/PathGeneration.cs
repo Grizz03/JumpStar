a4acfbe189557347822e2e6aa748cf0f9a401bd0
grizz03 <sawilson1110@yahoo.com>
2021-04-29 23:09:27 -0700

    added world movement

---
+++ Assets/Scripts/PathGeneration.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGeneration : MonoBehaviour
+{
+    public GameObject pathObject;
+
+    public Transform thresholdPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < thresholdPoint.position.z)
+        {
+            //copy pathObject if true
+            Instantiate(pathObject, transform.position, transform.rotation);
+            transform.position += new Vector3(0f, 0f, 2f);
+        }
+    }
+}
---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+
+    static public float zPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zPosition = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
